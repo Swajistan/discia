a36060a7e4c3b0262ed0e5c429032e04fb0079a4
Kefendy <67112181+Kefendy@users.noreply.github.com>
2021-12-21 19:39:41 +0300

    İlk dosya yüklendi.

---
+++ discia.py
@@ -0,0 +1,20 @@
+islem = input("Yapmak istediginiz islemi yaziniz: [yukleme] [silme] [indirme] [gonderme] ")
+if islem == "yukleme" :
+	x = float(input("Yuklemek istediginiz dosyanin adini yaziniz: "))
+	y = float(input("Yuklemek icin gereken 2. dosyanin adini yaziniz: "))
+	print(x + y)
+elif islem == "indirme" :
+	x = float(input("Indirmek istediginiz dosyayin adini yaziniz: "))
+	y = float(input("Indirmek icin gereken 2. dosyanin adini yaziniz: "))
+	print(x - y)
+elif islem == "silme" :
+	x = float(input("Silmek istediginiz dosyanin adini yaziniz: "))
+	y = float(input("Silmek icin gereken 2. dosyanin adini yaziniz: "))
+	print(x / y)
+elif islem == "gonderme" :
+	x = float(input("Gondermek istediginiz dosyanin adini yaziniz: "))
+	y = float(input("Gondermek icin gereken 2. dosyanin adini giriniz: "))
+	print(x * y)
+
+else :
+	print("Gecersiz islem girdiniz. Lütfen tekrar deneyiniz.")
